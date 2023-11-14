a60c2c7a8704375f9b4ec347199991f34cb3a96e
tustas123 <150567194+tustas123@users.noreply.github.com>
2023-11-14 01:33:26 -0600

    commit 4

--- PWA/sw.js
+++ PWA/sw.js
@@ -1,60 +1,87 @@
-;
-//asignar un nombre y versión al cache
-const CACHE_NAME = 'v1_pwa_app_cache',
-  urlsToCache = [
-    './',
-    'index.html',
-    'css/style.css',
-    'js/script.js',
-    'img/mainlogo.png',
-    'img/favicon.png'
-  ]
+// Asignar un nombre y versión al caché
+const CACHE_NAME = 'v1_pwa_app_cache';
+const urlsToCache = [
+  './',
+  'index.html',
+  'css/style.css',
+];
 
-//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+// Durante la fase de instalación, generalmente se almacenan en caché los activos estáticos
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        return cache.addAll(urlsToCache)
-          .then(() => self.skipWaiting())
+        return Promise.all(
+          urlsToCache.map(url => {
+            return fetch(url)
+              .then(response => {
+                if (!response.ok) {
+                  throw new Error(`Failed to fetch: ${url}`);
+                }
+                return cache.put(url, response);
+              })
+              .catch(err => console.error(`Error al almacenar en caché ${url}`, err));
+          })
+        )
+        .then(() => self.skipWaiting())
+        .catch(err => console.error('Error en la instalación del cache', err));
       })
-      .catch(err => console.log('Falló registro de cache', err))
-  )
-})
+  );
+});
 
-//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+// Una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
 self.addEventListener('activate', e => {
-  const cacheWhitelist = [CACHE_NAME]
+  const cacheWhitelist = [CACHE_NAME];
 
   e.waitUntil(
     caches.keys()
       .then(cacheNames => {
         return Promise.all(
           cacheNames.map(cacheName => {
-            //Eliminamos lo que ya no se necesita en cache
             if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName)
+              return caches.delete(cacheName);
             }
           })
-        )
+        );
       })
-      // Le indica al SW activar el cache actual
       .then(() => self.clients.claim())
-  )
-})
+      .catch(err => console.error('Error en la activación del cache', err))
+  );
+});
 
-//cuando el navegador recupera una url
+// Cuando el navegador recupera una URL
 self.addEventListener('fetch', e => {
-  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  // Responder ya sea con el objeto en caché o continuar y buscar la URL real
   e.respondWith(
     caches.match(e.request)
       .then(res => {
         if (res) {
-          //recuperar del cache
-          return res
+          // Recuperar del caché
+          return res;
         }
-        //recuperar de la petición a la url
+        // Recuperar de la petición a la URL
         return fetch(e.request)
-      }).catch(err => console.log('Falló algo al solicitar recursos', err))
-  )
-})
+          .then(response => {
+            // Verificar si la respuesta es válida antes de almacenarla en el caché
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clonar la respuesta para que pueda ser almacenada en el caché y servida
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(e.request, responseToCache);
+              });
+
+            return response;
+          })
+          .catch(err => {
+            console.error('Falló algo al solicitar recursos', err);
+            // Puedes devolver una respuesta de fallback aquí si deseas
+          });
+      })
+      .catch(err => console.error(`Error al recuperar del caché ${e.request.url}`, err))
+  );
+});
